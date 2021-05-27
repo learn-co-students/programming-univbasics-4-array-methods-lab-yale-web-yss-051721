3dee388e94be7a7255fcdb429e114b315ea3de4d
Lindsay Chen <lindsay.chen@yale.edu>
2021-05-27 19:19:07 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,37 @@
 def using_include(array, element)
-
+  array.include? (element)
 end
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+using_include array, "wow"
+using_include array, "Tardis"
+
 
 def using_sort(array)
-
+  array.sort 
 end
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+p using_sort (array)
+
 
 def using_reverse(array)
-
+  array.reverse 
 end
+p using_reverse (array)
+
 
 def using_first(array)
-
+  array.first
 end
+using_first (array)
+
 
 def using_last(array)
-
+  array.last 
 end
+using_last (array)
+
 
 def using_size(array)
-
+  array.size 
 end
+using_size (array)
