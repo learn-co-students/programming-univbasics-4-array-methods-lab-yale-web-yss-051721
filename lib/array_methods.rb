895510136e4ec26a6594635ce935125dc834eadf
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-29 20:06:21 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array = ["I", "Jk", "wow"]
+  array.sort
 end
 
 def using_reverse(array)
-
+  array = ["wow", "Jk", "arrays!"]
+  array.reverse
 end
 
 def using_first(array)
-
+  array = ["wow", "Jk", "arrays!"]
+  array.first
 end
 
 def using_last(array)
-
+  array = ["wow", "Jk", "arrays!"]
+  array.last
 end
 
 def using_size(array)
-
+   array = ["wow", "Jk", "arrays!", "wow", "Jk", "arrays!"]
+   array.size
 end
